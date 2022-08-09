766f86544c40e6b857b4095125098a487f69cff3
takassuzuki <takassuzuki.development.0227@gmail.com>
2022-08-09 22:59:49 +0900

    templateを作成

---
+++ template.py
@@ -0,0 +1,24 @@
+""" 
+Title：ABC086A-Product
+URL：https://atcoder.jp/contests/abs/tasks/abc086_a
+"""
+
+import io
+import sys
+
+# 問題のINPUTをここに入力する
+_INPUT = """\
+3 4
+1 21
+"""
+sys.stdin = io.StringIO(_INPUT)
+
+""" 
+回答はここから
+ """
+
+# 【共通】標準入力から値を取得する
+
+# ロジック記述
+
+# 【共通】標準出力
