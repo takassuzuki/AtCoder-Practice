903fb192e3d327622583dced08704cb1c0ae308b
takassuzuki <takassuzuki.development.0227@gmail.com>
2022-08-13 11:49:43 +0900

    タプルのサンプルを追加

---
+++ tuple.py
@@ -0,0 +1,14 @@
+""" 
+タプル
+"""
+# タプルはリストと違い、要素の追加/変更/削除ができない。
+
+# タプルの作成
+tuple_a = ('dog', 'cat', 'bird')
+print(tuple_a)
+
+# タプルの連結
+tuple_b = (10,20)
+tuple_c = ('A','B')
+tuple_d = tuple_b + tuple_c
+print(tuple_d)
